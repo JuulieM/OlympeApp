34bd98a5377301d5b41fd5d6f5ab483b14d2d598
yanngodeau <yann.godeau@gmail.com>
2019-02-27 16:12:54 +0100

    - New style from Zeplin - Fix date

--- app/src/main/java/com/example/olympeapp/MainActivity.kt
+++ app/src/main/java/com/example/olympeapp/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.olympeapp
 
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,17 +23,9 @@ class MainActivity : AppCompatActivity() {
      * @return Current date in local format
      */
     fun getCurrentDate(): String {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val current = LocalDateTime.now()
-            val formatter = SimpleDateFormat("EEEE dd MMMM", Locale.FRANCE)
-            var answer: String = formatter.format(current)
-            answer
-        } else {
-            var date = Date();
-            val formatter = SimpleDateFormat("EEEE dd MMMM", Locale.FRANCE)
-            val answer: String = formatter.format(date)
-            answer
-        }
+        val date = Date()
+        val formatter = SimpleDateFormat("EEEE dd MMMM", Locale.FRANCE)
+        return formatter.format(date)
     }
 
 }
